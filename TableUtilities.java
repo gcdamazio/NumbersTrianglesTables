bd8ab37517b585aa7838da5421a3fe798ec1d4ae
gcdamazio <damaziogabrielle@gmail.com>
2024-10-28 17:03:14 -0400

    Zip files

---
+++ TableUtilities.java
@@ -0,0 +1,18 @@
+package io.zipcoder.microlabs.mastering_loops;
+
+public class TableUtilities {
+    public static String getMultiplicationTable(int tableSize) {
+
+        return null;
+    }
+
+    public static String getSmallMultiplicationTable() {
+
+        return getMultiplicationTable(5);
+    }
+
+    public static String getLargeMultiplicationTable() {
+
+        return getMultiplicationTable(10);
+    }
+}
