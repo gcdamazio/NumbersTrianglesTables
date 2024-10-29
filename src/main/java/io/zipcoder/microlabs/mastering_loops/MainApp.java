2a0901bfe8e67bd413b6f33233602d11b550d248
gcdamazio <damaziogabrielle@gmail.com>
2024-10-28 22:54:48 -0400

    Finished NumberUtilities.java all tests passed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
@@ -7,5 +7,6 @@ import java.util.Scanner;
  */
 public class MainApp {
     public static void main(String[] args) {
+
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,39 +3,85 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
 
+    //return a String concatenation of all values, raised to the specified exponent, between start up to and not including stop, incrementing by the specified step.
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String range = "";
+        int product;
+        for (int i = start; i < stop; i += step) {
+            product = (int) Math.pow(i,exponent);
+            range = range + product;
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i += step) {
+            range = range + i;
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i++) {
+            range = range + i;
+        }
+        return range;
     }
 
     public static String getRange(int stop) {
-        return null;
+        String range = "";
+        for (int i = 0; i < stop; i++) {
+            range = range + i;
+        }
+        return range;
     }
+
+
+
+
 
     /*
      * natural break
      */
 
-    public static boolean isNumberEven(int toTest) { return false; }
-    public static boolean isNumberOdd(int toTest) { return false; }
+    public static boolean isNumberEven(int toTest) {
+        return ((toTest % 2) == 0);
+    }
+
+    public static boolean isNumberOdd(int toTest) {
+        return ((toTest % 2) == 1);
+    }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String justEven = "";
+        for (int i = start; i < stop; i++) {
+            if (isNumberEven(i)) {
+                justEven = justEven + i;
+            }
+        }
+        return (justEven);
     }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String justOdd = "";
+        for (int i = start; i < stop; i++) {
+            if (isNumberOdd(i)) {
+                justOdd = justOdd + i;
+            }
+        }
+        return (justOdd);
     }
 
+    //return a String concatenation of all values squared between start up to and not including stop.
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String range = "";
+        int square = 0;
+        for (int i = start; i < stop; i += step) {
+            square= i * i;
+            range = range + square;
+        }
+        return range;
     }
-
 }
